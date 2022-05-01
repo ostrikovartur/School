49a4e475b253ec2a03543c0f85e8839891b837c0
АРТУР <@DESKTOP-0H98069>
2022-05-01 16:50:57 +0300

    Add project files.

---
+++ School/Program.cs
@@ -0,0 +1,157 @@
+﻿using School;
+
+
+Address malynivka = new Address
+{
+    Country = "Ukraine",
+    City = "Malynivka",
+    Street = "Вул. Шкільна 1",
+    PostalCode = 22360
+};
+
+Employee director = new Employee
+{
+    FirstName = "Володимир",
+    LastName = "Сиченко"
+};
+
+School.School malynivskaSchool = new School.School
+{
+    Address = malynivka,
+    Name = "Малинівська школа #1",
+    OpeningDate = new DateOnly(2002, 1, 1),
+    Floors = new List<Floor>(),
+    Rooms = new List<Room>(),
+    Director = director,
+    Employees = new List<Employee>()
+};
+
+Floor firstFloor = new Floor
+{
+    Number = 1,
+    Rooms = new List<Room>()
+};
+
+malynivskaSchool.Floors.Add(firstFloor);
+
+Floor secondFloor = new Floor
+{
+    Number = 2,
+    Rooms = new List<Room>()
+};
+malynivskaSchool.Floors.Add(secondFloor);
+
+Floor thirdFloor = new Floor
+{
+    Number = 3,
+    Rooms = new List<Room>()
+};
+malynivskaSchool.Floors.Add(thirdFloor);
+
+Room mathRoom = new Room
+{
+    Number = 101,
+    Type = RoomType.Regular | RoomType.Math,
+    Floor = firstFloor
+};
+
+Room bioRoom = new Room
+{
+    Number = 202,
+    Type = RoomType.Regular | RoomType.Biology,
+    Floor = secondFloor
+};
+Room informRoom = new Room
+{
+    Number = 103,
+    Type = RoomType.Regular | RoomType.Informatic,
+    Floor = firstFloor
+};
+Room literatureRoom = new Room
+{
+    Number = 204,
+    Type = RoomType.Regular | RoomType.Literature,
+    Floor = secondFloor
+};
+Room gymRoom = new Room
+{
+    Number = 100,
+    Type = RoomType.Regular | RoomType.Gym,
+    Floor = thirdFloor
+};
+Room physicsRoom = new Room
+{
+    Number = 203,
+    Type = RoomType.Physics,
+    Floor = secondFloor
+};
+Room hallRoom = new Room
+{
+    Number = 200,
+    Type = RoomType.Hall,
+    Floor = secondFloor
+};
+
+
+malynivskaSchool.Rooms.Add(mathRoom);
+malynivskaSchool.Rooms.Add(bioRoom);
+malynivskaSchool.Rooms.Add(informRoom);
+malynivskaSchool.Rooms.Add(literatureRoom);
+malynivskaSchool.Rooms.Add(gymRoom);
+malynivskaSchool.Rooms.Add(physicsRoom);
+malynivskaSchool.Rooms.Add(hallRoom);
+
+firstFloor.Rooms.Add(mathRoom);
+
+firstFloor.Rooms.Add(informRoom);
+
+secondFloor.Rooms.Add(bioRoom);
+
+secondFloor.Rooms.Add(literatureRoom);
+
+thirdFloor.Rooms.Add(gymRoom);
+
+secondFloor.Rooms.Add(physicsRoom);
+
+secondFloor.Rooms.Add(hallRoom);
+
+    //class Malynovska
+    //{
+    //    string officialName = "Малинівська школа #1";
+    //    string address = "Вул. Шкільна 1";
+    //    int Floors = 2;
+    //    int yearOfOpening = 2002;
+    //}
+    //class Math
+    //{
+    //    int maxPupils = 15;
+    //    int Dimensions = 5; //розміри у метрах квадратних
+    //    string Floor = Floor2;
+    //}
+    //class Hall
+    //{
+    //    int maxPupils = 80;
+    //    int Dimensions = 15;
+    //    string Floor = Floor2;
+    //}
+    //class Medical
+    //{
+    //    public int maxPupils = 5;
+    //    public int Dimensions = 3;
+    //    public string Floor = Floor2;
+    //}
+    //class Dining
+    //{
+    //    public int maxPupils = 30;
+    //    public int Dimensions = 10;
+    //    public string Floor = Floor1;
+    //}
+    //class Floor1
+    //{
+    //    int Dimensions = 40;
+    //}
+    //class Floor2
+    //{
+    //    int Dimensions = 40;
+    //}
+//}
---
+++ School/School.cs
@@ -0,0 +1,60 @@
+﻿namespace School;
+
+class School
+{
+    //public Malynovska SchoolName = Malynovska;
+    //public string classMath = Math;
+    //public string schoolHall = Hall;
+    //public string medicalClass = Medical;
+    //public string diningRoom = Dining;
+    //public string schoolFloor1 = Floor1;
+    //public string schoolFloor2 = Floor2;
+    public Address Address { get; set; }
+    public string Name { get; set; }
+    public ICollection<Room> Rooms { get; set; }
+    public DateOnly OpeningDate { get; set; }
+    public ICollection<Floor> Floors { get; set; }
+    public Employee Director { get; set; }
+    public ICollection<Employee> Employees { get; set; }
+}
+
+public class Address
+{
+    public string City { get; set; }
+    public string Country { get; set; }
+    public string Street { get; set; }
+    public int PostalCode { get; set; }
+}
+
+public class Employee
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+}
+
+public class Room
+{
+    public int Number { get; set; }
+    public RoomType Type { get; set; }
+    public Floor Floor { get; set; }
+}
+
+public class Floor
+{
+    public int Number { get; set; }
+    public ICollection<Room> Rooms { get; set; }
+}
+
+[Flags]
+public enum RoomType
+{
+    Regular = 1,
+    Math = 2,
+    Biology = 4,
+    Literature = 8,
+    Informatic = 16,
+    Gym = 32,
+    Physics = 64,
+    Hall = 128,
+}
