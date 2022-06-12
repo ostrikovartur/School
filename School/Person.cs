d5e68eafbdbaaf23c8fbd5e6b0e0e3b5d3454af5
АРТУР <ostrikovartur21@gmail.com>
2022-06-12 12:21:03 +0300

    Create class Person

---
+++ School/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace School;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+}
