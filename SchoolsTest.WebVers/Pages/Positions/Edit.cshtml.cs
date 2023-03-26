90ceca461ebfce6ccf56b0f7847de1ea8bc1874c
ostrikovartur <ostrikovartur21@gmail.com>
2023-03-26 15:02:05 +0300

    Add positions in web

--- SchoolsTest.WebVers/Pages/Positions/Edit.cshtml.cs
+++ SchoolsTest.WebVers/Pages/Positions/Edit.cshtml.cs
@@ -1,12 +1,67 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SchoolsTest.Models.Interfaces;
+using SchoolsTest.Models;
 
-namespace SchoolsTest.WebVers.Pages.Positions
+namespace SchoolsTest.WebVers.Pages.Positions;
+
+public class Edit : BasePageModel
 {
-    public class EditModel : PageModel
+    private readonly IRepository<Position> _positionRepository;
+    public IEnumerable<Position> Positions { get; set; }
+    public IEnumerable<School> Schools { get; set; }
+    public School School { get; set; }
+    public Position Position { get; set; }
+
+    public Edit(IRepository<Position> repository)
     {
-        public void OnGet()
+        _positionRepository = repository;
+    }
+    public IActionResult OnGet(int id)
+    {
+        var schoolId = GetSchoolId();
+
+        if (schoolId == null)
         {
+            return Redirect("/schools");
         }
+
+        Position = _positionRepository.Get(id);
+
+        if (Position is null)
+        {
+            return NotFound("Position is not found");
+        }
+
+        return Page();
+    }
+    public IActionResult OnPostUpdate(Position position)
+    {
+        var positionId = position.Id;
+
+        var positionToUpdate = _positionRepository.Get(positionId);
+        if (positionToUpdate is null)
+        {
+            return NotFound("Position is not found");
+        }
+
+        positionToUpdate.Name = position.Name;
+
+        _positionRepository.Update(positionToUpdate);
+        return Redirect($"/positions");
+    }
+
+    public IActionResult OnPostDelete(Position position)
+    {
+        var positionId = position.Id;
+
+        var positionToUpdate = _positionRepository.Get(positionId);
+        if (positionToUpdate is null)
+        {
+            return NotFound("Position is not found");
+        }
+
+        _positionRepository.Delete(positionToUpdate);
+        return Redirect($"/positions");
     }
 }
