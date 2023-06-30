cb456a427277084a120937ed5b1ccd4095b742bc
ostrikovartur <ostrikovartur21@gmail.com>
2023-06-30 17:04:22 +0300

    Add roomDto

---
+++ SchoolsTest.WebVers/ViewModels/RoomDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SchoolsTest.WebVers.ViewModels;
+
+public class RoomDto
+{
+    public int Id { get; set; }
+    public int Number { get; set; }
+    public int[] RoomTypeIds { get; set; }
+}
