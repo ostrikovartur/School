3140c381fc46a4ba8108d49195a9f64b0b7d04ea
ostrikovartur <ostrikovartur21@gmail.com>
2023-06-30 16:49:49 +0300

    Add new methods

--- School.Data/RoomRepository.cs
+++ School.Data/RoomRepository.cs
@@ -13,6 +13,27 @@ public class RoomRepository : Repository<Room>, IRoomRepository
 
     public Room? GetRoomWithRoomTypes(int roomId)
     {
-        return _dbContext.Rooms.Include(r => r.RoomTypes).FirstOrDefault(r => r.Id == roomId);
+        return _dbContext.Set<Room>()
+            .Include(r => r.RoomTypes)
+            .FirstOrDefault(r => r.Id == roomId);
     }
+    public async Task<IEnumerable<Room>> GetRoomsWithTypes(int schoolId)
+    {
+        var rooms = await _dbContext.Set<Room>()
+            .Include(r => r.RoomTypes)
+            .Where(r => r.Floor.SchoolId == schoolId)
+            .ToListAsync();
+
+        return rooms;
+    }
+    //public async Task<IEnumerable<Room>> GetFloorId(int schoolId)
+    //{
+    //    var floorId = await _dbContext
+    //}
+    //protected int? GetSchoolId()
+    //{
+    //    var schoolIdStr = HttpContext.Request.Cookies["SchoolId"];
+
+    //    return int.TryParse(schoolIdStr, out var schoolId) ? schoolId : null;
+    //}
 }
