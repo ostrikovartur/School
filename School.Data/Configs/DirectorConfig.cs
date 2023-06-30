7eddcf8bf6a60a6292348ccbc9ebe86d091c23cf
ostrikovartur <ostrikovartur21@gmail.com>
2023-06-30 16:44:37 +0300

    Delete Director and Teacher employee

---
+++ School.Data/Configs/DirectorConfig.cs
@@ -0,0 +1,19 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Metadata.Builders;
+//using SchoolsTest.Models;
+
+//namespace SchoolsTest.Data.Configs;
+
+//public class DirectorConfig : IEntityTypeConfiguration<Director>
+//{
+//    public void Configure(EntityTypeBuilder<Director> builder)
+//    {
+//        builder.ToTable("Director");
+
+//        builder.HasBaseType(typeof(Employee));
+
+//        builder.HasOne(t => t.School)
+//            .WithOne(t => t.Director);
+
+//    }
+//}
---
+++ SchoolsTest.WebVers/Pages/Employees/TeacherDto.cs
@@ -0,0 +1,8 @@
+﻿using SchoolsTest.WebVers.ViewModels;
+
+namespace SchoolsTest.WebVers.Pages.Employees;
+
+public class TeacherDto : EmployeeDto
+{
+	public int TeacherId { get; set; }
+}
