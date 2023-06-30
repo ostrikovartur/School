7b08863f122c626136262a7fa38837b947053ed2
ostrikovartur <ostrikovartur21@gmail.com>
2023-06-30 16:48:52 +0300

    Remove dbSets and add migrations

--- School.Data/AppDbContext.cs
+++ School.Data/AppDbContext.cs
@@ -1,29 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Internal;
 using SchoolsTest.Models;
 
 namespace SchoolsTest.Data;
 
 public class AppDbContext : DbContext
 {
-    public DbSet<School> Schools { get; set; } = null!;
-    public DbSet<Address> Address { get; set; } = null!;
-    public DbSet<Floor> Floors { get; set; } = null!;
-    public DbSet<Room> Rooms { get; set; } = null!;
-    //public DbSet<Employee> Employees { get; set; } = null!;
-    //public DbSet<EmployeePosition> EmployeePositions { get; set; } = null !;
-
-    //public DbSet<Director> Directors { get; set; } = null!;
-    //public DbSet<Teacher> Teachers { get; set; } = null!;
-    public DbSet<Student> Students { get; set; } = null!;
-    //public DbSet<Position> Positions { get; set; } = null !;
-    public Models.School? CurrentSchool { get; set; }
-
-    //public Models.School? CurrentFloor { get; set; }
+    public School? CurrentSchool { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
     {
-        Database.EnsureCreated();
+        //Database.EnsureCreated();
+        Database.GetMigrations();
+        Database.Migrate();
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -35,25 +26,8 @@ public class AppDbContext : DbContext
         modelBuilder.ApplyConfiguration(new Configs.FloorConfig());
         modelBuilder.ApplyConfiguration(new Configs.RoomConfig());
         modelBuilder.ApplyConfiguration(new Configs.RoomTypeConfig());
-        //modelBuilder.ApplyConfiguration(new Configs.EmployeePositionConfig());
-        //modelBuilder.ApplyConfiguration(new Configs.DirectorConfig());
         modelBuilder.ApplyConfiguration(new Configs.EmployeeConfig());
         modelBuilder.ApplyConfiguration(new Configs.PositionConfig());
-        //modelBuilder.ApplyConfiguration(new Configs.TeacherConfig());
         modelBuilder.ApplyConfiguration(new Configs.StudentConfig());
-
-        //modelBuilder.Entity<Employee>()
-        //    .HasMany(t => t.Positions)
-        //    .WithMany(t => t.Employees)
-        //    .UsingEntity(j => j.ToTable("EmployeePosition"));
     }
-    //public void SetSchools(IEnumerable<Models.School> schools)
-    //{
-    //    _schools = schools.ToList();
-    //}
-
-    //public void AddSchool(Models.School school)
-    //{
-    //    _schools.Add(school);
-    //}
 }
