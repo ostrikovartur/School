fc94784158e9c7b18a4bf8e0a9d497424371256d
ostrikovartur <ostrikovartur21@gmail.com>
2023-08-10 12:49:54 +0300

    Add methods

--- School.Models/Room.cs
+++ School.Models/Room.cs
@@ -7,17 +7,40 @@ public class Room : BaseEntity
     public int Number { get; set; }
     public int FloorId { get; set; }
     public Floor Floor { get; set; }
+    public int[] RoomTypeIds => RoomTypes.Select(x => x.Id).ToArray();
 
     public Room()
     {
 
     }
 
-    public Room(int number, IEnumerable<RoomType> type, Floor floor)
+    public Room(int number, ICollection<RoomType> type, Floor floor)
     {
         Number = number;
-        RoomTypes = new HashSet<RoomType>(type);
+        RoomTypes = type;
         Floor = floor;
+    }
+    public Room(int number, Floor floor)
+    {
+        Number = number;
+        Floor = floor;
+    }
+    public void SetNumber(int number)
+    {
+        Number = number;
+    }
+    public void SetFloor(int floorId)
+    {
+        FloorId = floorId;
+    }
+    public void SetRoomTypes(ICollection<RoomType> roomTypes)
+    {
+        RoomTypes.Clear();
+
+        foreach (var roomType in roomTypes)
+        {
+            RoomTypes.Add(roomType);
+        }
     }
 
     public override string ToString()
