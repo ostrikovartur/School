6844e8923a8a7ab073cd53cb68fe8f4ef4f24ba8
ostrikovartur <ostrikovartur21@gmail.com>
2023-11-05 15:46:55 +0200

    add schoolId filters

---
+++ SchoolsTest.API/SchoolIdChecker.cs
@@ -0,0 +1,81 @@
+﻿using Azure;
+using Azure.Core;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Tokens;
+using SchoolsTest.Data;
+using SchoolsTest.Data.DTOs;
+using SchoolsTest.Models.Interfaces;
+using System.Runtime.CompilerServices;
+
+namespace SchoolsTest.API;
+
+public class SchoolIdChecker
+{
+    private readonly RequestDelegate _next;
+
+    private readonly ISchoolRepository _repository;
+
+    private readonly AppDbContext _dbContext;
+
+    public SchoolIdChecker(RequestDelegate next)
+    {
+        _next = next;
+    }
+    
+    public async Task InvokeAsync(HttpContext context, int id)
+    {
+        //var schoolId = _repository.Get(id);
+
+        //if (schoolId is null)
+        //{
+        //    Results.NotFound("SchoolId not found, create school before create this object");
+        //}
+
+        var schoolIdQuery = context.Request.Query["schoolId"];
+
+        if (!string.IsNullOrWhiteSpace(schoolIdQuery))
+        {
+            if(_dbContext == null)
+            {
+                await context.Response.WriteAsJsonAsync("Bad request");
+                await _next(context);
+            }
+        }
+
+        var schoolIdBody = context.Request.Body;
+
+        if(schoolIdBody != null)
+        {
+            if (_dbContext == null)
+            {
+                await context.Response.WriteAsJsonAsync("Bad request");
+                await _next(context);
+            }
+        }
+
+        var schoolIdHeaders = context.Request.Headers;
+
+        if (!schoolIdHeaders.IsNullOrEmpty())
+        {
+            if (_dbContext == null)
+            {
+                await context.Response.WriteAsJsonAsync("Bad request");
+                await _next(context);
+            }
+        }
+
+        //var school = await _repository.Get(schoolId);
+
+        await _next(context);
+    }
+
+}
+public static class SchoolIdCheckerExtensions
+{
+    public static IApplicationBuilder UseSchoolIdChecker(
+        this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<SchoolIdChecker>();
+    }
+}
---
+++ SchoolsTest.API/SchoolIdExistsFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolsTest.Data.DTOs;
+using SchoolsTest.Models.Interfaces;
+
+namespace SchoolsTest.API;
+
+public class SchoolIdExistsFilter : IEndpointFilter 
+{
+    public ISchoolRepository _schoolRepository;
+
+    public SchoolIdExistsFilter (ISchoolRepository schoolRepository)
+    {
+        _schoolRepository = schoolRepository;
+    }
+
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context,
+        EndpointFilterDelegate next)
+    {
+        var schoolIdRoute = context.HttpContext.Request.RouteValues["schoolId"];
+
+        _ = int.TryParse(schoolIdRoute.ToString(), out int schoolId);
+
+        var school = await _schoolRepository.Get(schoolId);
+        
+        if (string.IsNullOrEmpty(schoolId.ToString()))
+        {
+            return Results.BadRequest("SchoolId not provided");
+        }
+
+        if (school is null)
+        {
+            return Results.NotFound("School not found(filter)");
+        }
+        return await next(context);
+    }
+}
