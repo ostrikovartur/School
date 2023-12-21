25c43af5b73ecb4fc0bd5feddf523553d13239dc
ostrikovartur <ostrikovartur21@gmail.com>
2023-12-21 21:27:12 +0200

    add confirm email handler

---
+++ SchoolsTest.API/User/ConfirmEmail.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SchoolsTest.Models.Constants;
+using System.Security.Claims;
+
+namespace SchoolsTest.API.User;
+
+public class ConfirmEmail
+{
+    public static async Task<IResult> Handle(
+        UserManager<IdentityUser<int>> userManager,
+        [FromBody] ConfirmEmailRequest authRequest)
+    {
+        var user = await userManager.FindByNameAsync(authRequest.UserName);
+
+        if (user is null)
+        {
+            return Results.NotFound($"User {authRequest.UserName} not found");
+        }
+
+        var result = await userManager.ConfirmEmailAsync(user, authRequest.ConfirmToken);
+
+        if (!result.Succeeded)
+        {
+            return Results.Problem(string.Join(";", result.Errors.Select(e => $"{e.Code}: {e.Description}")));
+        }
+
+        return Results.Ok();
+    }
+}
