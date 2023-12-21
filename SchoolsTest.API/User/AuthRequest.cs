4bcce9da403cceb50ee40dfcce0f1777330f8fe6
ostrikovartur <ostrikovartur21@gmail.com>
2023-12-21 21:27:00 +0200

    add few authentication requests models

--- SchoolsTest.API/User/AuthRequest.cs
+++ SchoolsTest.API/User/AuthRequest.cs
@@ -1,3 +1,21 @@
 ﻿namespace SchoolsTest.API.User;
 
-public sealed record AuthRequest(string UserName, string Password);
+public record AuthRequest(string UserName, string Password, string Email);
+
+public sealed record ChangePasswordRequest(string NewPassword, string ConfirmedPassword, string UserName, string Password, string Email)
+    : AuthRequest(UserName, Password, Email)
+{
+    public bool IsPasswordConfirmed()
+    {
+        return NewPassword == ConfirmedPassword;
+    }
+}
+
+public sealed record SetNewPasswordRequest(string NewPassword, string ConfirmedPassword, string UserName, string ResetToken, string Email)
+{
+    public bool IsPasswordConfirmed()
+    {
+        return NewPassword == ConfirmedPassword;
+    }
+}
+public sealed record ConfirmEmailRequest(string UserName, string ConfirmToken);
