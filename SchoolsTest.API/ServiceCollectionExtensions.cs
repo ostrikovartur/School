b1f2704454383fb2057934ee8c3455b48b1b576f
ostrikovartur <ostrikovartur21@gmail.com>
2023-12-21 21:30:39 +0200

    create new claims and permissions

--- SchoolsTest.API/ServiceCollectionExtensions.cs
+++ SchoolsTest.API/ServiceCollectionExtensions.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
-using SchoolsTest.API;
 using SchoolsTest.Data;
+using SchoolsTest.Models.Constants;
+using System.Security.Claims;
 
 public static class ServiceCollectionExtensions
 {
@@ -42,12 +43,33 @@ public static class ServiceCollectionExtensions
         {
             options.AddPolicy(Policies.SystemAdmin, policy =>
             {
-                policy.RequireClaim(ClaimNames.Admin, ClaimValues.SystemAdmin);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.ManageSchool);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.ManagePosition);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.ManageRoomType);
             });
 
             options.AddPolicy(Policies.SchoolAdmin, policy =>
             {
-                policy.RequireClaim(ClaimNames.Admin, ClaimValues.SchoolAdmin);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.ManageSchoolUsers);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.ManageEmployee);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.OperatePositionsInSchool);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.OperateRoomTypesInSchool);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.ManageFloor);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.ManageRoom);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.ManageStudent);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.FullInfoStudent);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.FullInfoEmployee);
+            });
+
+            options.AddPolicy(Policies.Authorized, policy =>
+            {
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.InfoSchool);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.InfoEmployee);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.InfoStudent);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.InfoRoom);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.InfoFloor);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.InfoPosition);
+                policy.RequireClaim(ClaimNames.Permission, ClaimValues.InfoRoomTypes);
             });
         });
 
